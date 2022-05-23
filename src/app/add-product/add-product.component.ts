2659e6058e6783770239b2a6f0f231ae91de9a03
leov <leo.verbeke@hotmail.fr>
2022-05-23 11:10:14 +0200

    send file work

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProductComponent implements OnInit {
 
-  constructor() { }
+  file: File | null = null;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
+
+
+
+  onFileSelected(event:any) {
+
+        console.log(event)
+
+        this.file = <File>event.target.files[0];
+        
+        let formData = new FormData();
+        
+        formData.append("image", this.file);
+
+        // console.log(this.file)
+
+        this.http.post('http://localhost:8000/admin/product/create', formData).subscribe(result => {
+            
+        console.log('addProduct', result);
+
+            // const upload$ = this.http.post("/api/thumbnail-upload", formData);
+
+            // upload$.subscribe();
+        })
+      
+    }
+
+  onSubmitAddP(value:any) {
+    console.log('addP', value);
+    
+        this.http.post('http://localhost:8000/admin/product/create', value).subscribe(result => {
+        console.log('addProduct', result);
+    })}
 
 }
