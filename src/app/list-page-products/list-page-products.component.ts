c9c8fa3008dd51f45c8600093216a3b4a4790496
leov <leo.verbeke@hotmail.fr>
2022-05-25 15:22:00 +0200

    panier show et add done

--- src/app/list-page-products/list-page-products.component.ts
+++ src/app/list-page-products/list-page-products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ListProducts } from '../models/product.model';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -10,8 +11,10 @@ import { Router } from '@angular/router';
 })
 export class ListPageProductsComponent implements OnInit {
 
+  $user_id: any = sessionStorage.getItem('id');
+
   @Input() product!: ListProducts;
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
   }
@@ -20,4 +23,16 @@ export class ListPageProductsComponent implements OnInit {
     this.router.navigateByUrl('/produits/:id')
   }
 
+  addToDb(productId:any) : void {
+
+    let addCartData = new FormData();
+
+    addCartData.append("product_id", productId);
+
+    addCartData.append("user_id", this.$user_id);
+
+    this.http.post('http://localhost:8000/cart/add', addCartData).subscribe(result => {
+        console.log('addProduct', result );
+  })
+}
 }
